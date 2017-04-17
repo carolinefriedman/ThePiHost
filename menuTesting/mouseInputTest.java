007faa022e4beb6cb508138b899c9570f39cc465
gpogoncheff <galenpogoncheff@me.com>
2017-04-17 12:32:57 -0600

    Update mouseInputTest.java
    Buttons working.  Just need to alter executable file paths when we have package ready.

--- menuTesting/mouseInputTest.java
+++ menuTesting/mouseInputTest.java
@@ -9,28 +9,69 @@ public class mouseInputTest implements MouseListener{
   public void mousePressed(MouseEvent e){
     int mouseX = e.getX();
     int mouseY = e.getY();
-    System.out.print(mouseX);
-    //play button
-    if((mouseY >= 85) && (mouseY <= 135)){
-        //pressed play button
-        try{
+
+    //buttons
+    if((mouseX >= 319) && (mouseX <= 693)){
+      if ((mouseY >= 235) && (mouseY <= 304)){
+        //try{
           //open 'program_name' runs program_name on mac system
-    			String cmd = "open pongGame.class";
+    			//String cmd = "open pongGame.class";
           //try to execute cmd
-    			Process p = Runtime.getRuntime().exec(cmd);
-    		}
-    		catch(IOException ex){
-    			ex.printStackTrace();
-    		}
+    			//Process p = Runtime.getRuntime().exec(cmd);
+          System.out.print("emulator button clicked\n");
+    		//}
+    		//catch(IOException ex){
+    		//	ex.printStackTrace();
+    		//}
       }
-    }
+      else if ((mouseY >= 363) && (mouseY <= 431)){
+            //pressed play button
+            //try{
+              //open 'program_name' runs program_name on mac system
+        			//String cmd = "open pongGame.class";
+              //try to execute cmd
+        			//Process p = Runtime.getRuntime().exec(cmd);
+              System.out.print("space invaders button clicked\n");
+        		//}
+        		//catch(IOException ex){
+        		//	ex.printStackTrace();
+        		//}
+      }
+      else if ((mouseY >= 491) && (mouseY <= 558)){
+            //pressed play button
+            //try{
+              //open 'program_name' runs program_name on mac system
+        			//String cmd = "open pongGame.class";
+              //try to execute cmd
+        			//Process p = Runtime.getRuntime().exec(cmd);
+              System.out.print("pong button clicked\n");
+        		//}
+        		//catch(IOException ex){
+        		//	ex.printStackTrace();
+        		//}
+      }
+      else if ((mouseY >= 613) && (mouseY <= 680)){
+            //pressed play button
+            //try{
+              //open 'program_name' runs program_name on mac system
+        			//String cmd = "open pongGame.class";
+              //try to execute cmd
+        			//Process p = Runtime.getRuntime().exec(cmd);
+              System.out.print("exit button clicked\n");
+        		//}
+        		//catch(IOException ex){
+        		//	ex.printStackTrace();
+        		//}
+       }
+     }
+   }
 
-    public void mouseReleased(MouseEvent e){
-    }
+   public void mouseReleased(MouseEvent e){
+   }
 
-    public void mouseEntered(MouseEvent e){
-    }
+   public void mouseEntered(MouseEvent e){
+   }
 
-    public void mouseExited(MouseEvent e){
-    }
+   public void mouseExited(MouseEvent e){
+   }
 }
