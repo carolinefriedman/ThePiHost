1bcd44ee0d5515118f0c5bbef9c2ac0cf0f1219d
gpogoncheff <galenpogoncheff@me.com>
2017-04-26 22:34:27 -0600

    Added file paths to launch games to main menu

--- menuTesting/mouseInputTest.java
+++ menuTesting/mouseInputTest.java
@@ -10,58 +10,37 @@ public class mouseInputTest implements MouseListener{
     int mouseX = e.getX();
     int mouseY = e.getY();
 
-    //buttons
+
     if((mouseX >= 319) && (mouseX <= 693)){
       if ((mouseY >= 235) && (mouseY <= 304)){
-        //try{
-          //open 'program_name' runs program_name on mac system
-    			//String cmd = "open pongGame.class";
-          //try to execute cmd
-    			//Process p = Runtime.getRuntime().exec(cmd);
-          System.out.print("emulator button clicked\n");
-    		//}
-    		//catch(IOException ex){
-    		//	ex.printStackTrace();
-    		//}
+        try{
+    			String cmd = "open Emulator/OpenEmu.app";
+    			Process p = Runtime.getRuntime().exec(cmd);
+    		}
+    		catch(IOException ex){
+    			ex.printStackTrace();
+    		}
       }
       else if ((mouseY >= 363) && (mouseY <= 431)){
-            //pressed play button
-            //try{
-              //open 'program_name' runs program_name on mac system
-        			//String cmd = "open pongGame.class";
-              //try to execute cmd
-        			//Process p = Runtime.getRuntime().exec(cmd);
-              System.out.print("space invaders button clicked\n");
-        		//}
-        		//catch(IOException ex){
-        		//	ex.printStackTrace();
-        		//}
+            try{
+        			String cmd = "java -jar SpaceDefenders/SpaceDefendersGame.jar";
+        			Process p = Runtime.getRuntime().exec(cmd);
+        		}
+        		catch(IOException ex){
+        			ex.printStackTrace();
+        		}
       }
       else if ((mouseY >= 491) && (mouseY <= 558)){
-            //pressed play button
-            //try{
-              //open 'program_name' runs program_name on mac system
-        			//String cmd = "open pongGame.class";
-              //try to execute cmd
-        			//Process p = Runtime.getRuntime().exec(cmd);
-              System.out.print("pong button clicked\n");
-        		//}
-        		//catch(IOException ex){
-        		//	ex.printStackTrace();
-        		//}
+            try{
+        			String cmd = "java -jar Tennis/TennisGame.jar";
+        			Process p = Runtime.getRuntime().exec(cmd);
+        		}
+        		catch(IOException ex){
+        			ex.printStackTrace();
+        		}
       }
       else if ((mouseY >= 613) && (mouseY <= 680)){
-            //pressed play button
-            //try{
-              //open 'program_name' runs program_name on mac system
-        			//String cmd = "open pongGame.class";
-              //try to execute cmd
-        			//Process p = Runtime.getRuntime().exec(cmd);
-              System.out.print("exit button clicked\n");
-        		//}
-        		//catch(IOException ex){
-        		//	ex.printStackTrace();
-        		//}
+            System.exit(0);
        }
      }
    }
