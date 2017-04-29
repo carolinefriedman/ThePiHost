daeaaa11612ec5f48c20f1cfa272636227482754
gpogoncheff <galenpogoncheff@me.com>
2017-04-29 16:46:08 -0600

    Moved a level up conditional intoa more appropriate method and added javadoc compatible documentation

--- tennisDB/src/code/Tennis.java
+++ tennisDB/src/code/Tennis.java
@@ -17,14 +17,19 @@ import java.time.format.DateTimeFormatter;
 
 import javax.swing.JFrame;
 
-/**
+/*
 *   Tennis is the first game that our team wrote.  In learning how to write
 *   a game in Java, we followed the tutorial at:
 *   https://www.youtube.com/watch?v=lE18VALSCAA
 *   The basic theory and flow follow what we learned in the tutorial, however,
 *   modifications were made to better suite the motives of our group.
 */
-
+/**
+ * This is the driver class for the tennis game.  It contains the main method,
+ * instances of the paddles and ball, instance of the game I/O handler, and
+ * the necessities for the JFrame and the accompanying display strategies.
+ *
+ */
 public class Tennis extends Canvas implements Runnable {
 
   JFrame frame;
@@ -39,20 +44,29 @@ public class Tennis extends Canvas implements Runnable {
   public final String TITLE = "Tennis";
   BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
   public int playerScore = 0;
+  /**
+   *  Every time the player scores 4 points (this score interval is arbitrary,
+   *  but while testing, 4 proved to be a good interval), make the ball move
+   *  faster. Also allow the paddles to move faster to keep up with ball movement.
+   */
   public int pScoreTracker = 0;
   public int compScore = 0;
   static boolean gameRunning = false;
+  /** Entered equals false if user has not proceeded past the game entry screen, true otherwise */
   static boolean entered = false;
-  
+
+  /**  mysql database username credentials */
+  private static final String USERNAME = "root";
+  /**  mysql database password credentials */
+  private static final String PASSWORD = "";
+  /**  mysql database connection credentials */
+  private static final String CONN_STRING = "jdbc:mysql://localhost/piproject";
+
   /**
-   *   mysql database connection credentials
+   * Creates instance of JFrame and instantiates the  player paddle to the left side of the screen,
+   * the computer paddle to the right side of the screen, and the ball to the center of the screen.
    */
-   private static final String USERNAME = "root";
-   private static final String PASSWORD = "";
-   private static final String CONN_STRING = "jdbc:mysql://localhost/piproject";
-
   public Tennis(){
-
     frame = new JFrame();
     this.setMinimumSize(gameSize);
     this.setPreferredSize(gameSize);
@@ -65,23 +79,27 @@ public class Tennis extends Canvas implements Runnable {
     frame.setTitle(TITLE);
     frame.setLocationRelativeTo(null);
 
+    /** user controlled paddle */
     player = new PlayerPaddle(10, 60);
+    /** automated paddle */
     compplayer = new ComputerPaddle(getWidth() - 25, 60);
     ball = new Ball((getWidth()/2), (getHeight()/2));
     io = new GameIO(this);
   }
 
+  /**
+   * Start point of execution for the thread.
+   *
+   * Reason that thread should sleep for a short amount of time:
+   * http://stackoverflow.com/questions/20634600/why-does-a-game-loop-need-to-sleep
+   * Smaller increments of sleep time were tried here, but the movement of figures
+   * on the screen became overly sensitive to keyboard input.
+   */
   public void run(){
     while (gameRunning){
       tick();
       render();
       try{
-        /**
-        * Reason that thread should sleep for a short amount of time:
-        * http://stackoverflow.com/questions/20634600/why-does-a-game-loop-need-to-sleep
-        * Smaller increments of sleep time were tried here, but the movement of figures
-        * on the screen became overly sensitive to keyboard input.
-        */
         Thread.sleep(5);
       }
       catch(Exception e){
@@ -89,28 +107,43 @@ public class Tennis extends Canvas implements Runnable {
       }
     }
   }
-
+  /**
+   * Method used to start executing gameplay instructions.
+   */
   public synchronized void start(){
     gameRunning = true;
     new Thread(this).start();
   }
 
   public static synchronized void stop(){
-    //gameRunning = false;
     System.exit(0);
   }
 
+  /**
+   * Driver method to call "tick" for the paddle and ball instances.
+   * In these methods is where updates are made to the coordinates and variables of these instances.
+   *
+   * This method also check the score of the player to determine if a "level-up" is due
+   */
   public void tick(){
     player.tick(this);
     compplayer.tick(this);
     ball.tick(this);
+    if (this.pScoreTracker % 4 == 0 && this.pScoreTracker > 0){
+        ball.speed += 1;
+        this.compplayer.paddleSpeed += 1;
+        this.player.paddleSpeed += 1;
+        this.pScoreTracker = 0;
+    }
   }
-
+  /**
+   * Used to display the graphics
+   *
+   * Set buffer stategy to triple buffering
+   * useful link describing multiple buffering techiques:
+   * https://en.wikipedia.org/wiki/Multiple_buffering
+  */
   public void render(){
-    /** Set buffer stategy to triple buffering
-     * useful link describing multiple buffering techiques:
-     * https://en.wikipedia.org/wiki/Multiple_buffering
-    */
     BufferStrategy buffStrat = getBufferStrategy();
 
     if (buffStrat == null){
@@ -134,18 +167,6 @@ public class Tennis extends Canvas implements Runnable {
       ball.render(graphics);
     }
 
-    /**
-    *  Every time the player scores 4 points (this score interval is arbitrary,
-    *  but while testing, 4 proved to be a good interval), make the ball move
-    *  faster. Also allow the paddles to move faster to keep up with ball movement.
-    */
-    if (this.pScoreTracker % 4 == 0 && this.pScoreTracker > 0){
-      ball.speed += 1;
-      this.compplayer.paddleSpeed += 1;
-      this.player.paddleSpeed += 1;
-      this.pScoreTracker = 0;
-    }
-
     if (this.compScore >= 10){
       GameOver(graphics, buffStrat);
     }
@@ -153,7 +174,13 @@ public class Tennis extends Canvas implements Runnable {
     graphics.dispose();
     buffStrat.show();
   }
-  
+  /**
+   * Used to both update the database with the score and date of the recently ended game
+   * and display the top three highscores in the game over screen.
+   *
+   * @param graphics main graphics instance declared in the Tennis class
+   * @throws SQLException
+   */
   private void dbUpdate(Graphics graphics) throws SQLException{
     Connection conn = null;
     PreparedStatement updateStmt = null;
@@ -218,13 +245,13 @@ public class Tennis extends Canvas implements Runnable {
     graphics.drawString("GAME OVER", WIDTH/2 - 50, HEIGHT/2 - 30);
     graphics.drawString("Total Score: " + playerScore, WIDTH/2 - 50, HEIGHT/2);
     graphics.drawString("Your High Scores:", WIDTH/2 - 70, HEIGHT/2 + 60);
-    
+
     try {
 		dbUpdate(graphics);
 	} catch (SQLException e) {
 		System.err.println(e);
 	}
-    
+
     graphics.dispose();
     buffer.show();
     gameRunning = false;
